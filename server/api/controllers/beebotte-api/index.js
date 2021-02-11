8b7d8262fd1e4c5824445f0ac4737fb48d19f86f
poruruba <44470781+poruruba@users.noreply.github.com>
2021-02-11 13:41:54 +0900

    Add files via upload

---
+++ server/api/controllers/beebotte-api/index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const HELPER_BASE = process.env.HELPER_BASE || '../../helpers/';
+const Response = require(HELPER_BASE + 'response');
+
+const BEEBOTTE_APIKEY = process.env.BEEBOTTE_APIKEY || '【BeebotteのAPIキー】';
+const BEEBOTTE_SECRETKEY = process.env.BEEBOTTE_SECRETKEY || '【Beebotteのシークレットキー】';
+
+const BASE_PATH = './public';
+const folders = [
+	{
+		name: 'dashboard',
+		path: '/dashboard/'
+	}
+];
+
+const fs = require('fs');
+const bbt = require('beebotte');
+const bclient = new bbt.Connector({
+  apiKey: BEEBOTTE_APIKEY,
+  secretKey: BEEBOTTE_SECRETKEY,
+});
+
+exports.handler = async (event, context, callback) => {
+	if( event.path == '/beebotte-auth' ){
+		var params = event.queryStringParameters;
+		var to_sign = params.sid + ':' + params.channel + '.' + params.resource + ':ttl=' + params.ttl + ':read=true:write=false';
+		var auth = bclient.sign( to_sign );
+
+		return new Response(auth);
+	}else
+	if( event.path == '/beebotte-update'){
+		var body = JSON.parse(event.body);
+		console.log(body);
+
+		var folder = folders.find(folder => folder.name == body.name);
+		if( !folder )
+			throw 'not found';
+
+		fs.writeFileSync(BASE_PATH + folder.path + 'layout.json', JSON.stringify(body.data, null, "\t") );
+
+		return new Response({ path : folder.path });
+	}else
+	if( event.path == '/beebotte-get'){
+		var body = JSON.parse(event.body);
+		console.log(body);
+
+		var folder = folders.find(folder => folder.name == body.name);
+		if( !folder )
+			throw 'not found';
+
+		var text = fs.readFileSync(BASE_PATH + folder.path + 'layout.json', { encoding: 'utf-8' })
+		return new Response({ path : folder.path, data: JSON.parse(text) });
+	}	
+};
---
+++ server/public/dashboard_edit/js/start.js
@@ -0,0 +1,164 @@
+'use strict';
+
+//var vConsole = new VConsole();
+
+const BBT_APIKEY = '【BeebotteのAPIキー】';
+const BBT_AUTH_ENDPOINT = '/beebotte-auth';
+
+var vue_options = {
+    el: "#top",
+    data: {
+        progress_title: '', // for progress-dialog
+
+        elements: [],
+        view_styles: [],
+        view_data: [],
+        bbt_value: [],
+        data_name: '',
+        image_path: null,
+        data_new: {
+            type: 'string',
+        },
+        data_new_color: '#000000'
+    },
+    computed: {
+    },
+    methods: {
+        toRGB: function(color){
+            return '#' + ('000000' + Number(color).toString(16)).slice(-6);
+        },
+        fromRGB: function(rgb){
+            return parseInt(this.data_new_color.substr(1), 16);
+        },
+        data_image_update: function(){
+            this.bbt.disconnect();
+            this.bbt.connect();
+
+            this.view_styles = [];
+            this.view_data = [];
+            for(var i = 0 ; i < this.elements.length ; i++ ){
+                var element = JSON.parse(JSON.stringify(this.elements[i]));
+                var style = {
+                    position: 'absolute',
+                    top: element.y + 'px',
+                    left: element.x + 'px',
+                    'font-size': element.size + 'px',
+                    color: this.toRGB(element.color),
+                };
+                if( element.align == 'center')
+                    style.transform = 'translateX(-50%)';
+                else if( element.align == 'right')
+                    style.transform = 'translateX(-100%)';
+
+                this.view_styles.push(style);
+                this.view_data.push(element);
+
+                if( element.type.startsWith('bbt_')){
+                    var topic = element.topic.split('/');
+                    this.bbt.subscribe( {channel: 'private-' + topic[0], resource: topic[1]}, (message) =>{
+                        var channel = message.channel;
+                        if( channel.startsWith('private-') )
+                            channel = message.channel.substr(8);
+                        var index = this.elements.findIndex(item => item.topic == (channel + '/' + message.resource) );
+                        if( index >= 0 )
+                           vue.$set(this.bbt_value, index, message.data);
+                    });
+                }
+                
+            }
+        },
+        data_close: function(){
+            if( this.btt )
+                this.bbt.disconnect();
+            this.view_data = [];
+            this.view_styles = [];
+            this.elements = [];
+            this.data_name = '';
+            this.image_path = null;
+        },
+        data_load: async function(){
+            var json = await do_post('/beebotte-get', { name: this.data_name });
+            this.elements = json.data;
+            this.image_path = json.path + 'background.png';
+            this.data_image_update();
+        },
+        data_save: async function(){
+            if( !this.data_name )
+                return;
+
+            var param = {
+                name: this.data_name,
+                data: this.elements
+            };
+            var json = await do_post('/beebotte-update', param);
+            alert('更新しました。');
+        },
+        call_data_delete: function(index){
+            if( !confirm('本当に削除しますか？') )
+                return;
+
+            this.elements.splice(index, 1);
+        },
+        call_data_update: function(index){
+            this.data_new = this.elements[index];
+            this.data_new_color = this.toRGB(this.data_new.color);
+            this.data_index = index;
+            this.dialog_open("#data_update");
+        },
+        do_data_update: async function(){
+            this.data_new.color = parseInt(this.data_new_color.substr(1), 16);
+            this.$set(this.elements, this.data_index, this.data_new);
+
+            this.dialog_close('#data_update');
+        },
+        call_data_new: function(){
+            this.data_new = {
+                type: 'string',
+            };
+            this.data_new_color = "#000000";
+            this.dialog_open('#data_new');
+        },
+        do_data_new: function(){
+            this.data_new.color = parseInt(this.data_new_color.substr(1), 16);
+            this.elements.push(this.data_new);
+
+            this.dialog_close('#data_new');
+        }
+    },
+    created: function(){
+    },
+    mounted: function(){
+        proc_load();
+
+        this.bbt = new BBT(BBT_APIKEY, { auth_endpoint: BBT_AUTH_ENDPOINT });
+    }
+};
+vue_add_methods(vue_options, methods_bootstrap);
+vue_add_components(vue_options, components_bootstrap);
+var vue = new Vue( vue_options );
+
+function do_get_data(url) {
+    return fetch(url, {
+        method: 'GET',
+    })
+    .then((response) => {
+        if (!response.ok)
+            throw 'status is not 200';
+        return response.json();
+    });
+}
+
+function do_post(url, body) {
+    const headers = new Headers({ "Content-Type": "application/json; charset=utf-8" });
+
+    return fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: headers
+    })
+    .then((response) => {
+        if (!response.ok)
+            throw 'status is not 200';
+        return response.json();
+    });
+}
